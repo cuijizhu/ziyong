0dbe3b37b3d8b000c71946538845514f4b2efcab
cuijizhu <60956208+cuijizhu@users.noreply.github.com>
2023-08-04 17:02:16 +0800

    Update iptv2.py

--- .github/workflows/iptv2.py
+++ .github/workflows/iptv2.py
@@ -1,36 +1,39 @@
 import requests
 import re
+import os
 
-def download_and_convert(url, filename):
+def download_and_convert(url):
     r = requests.get(url)
     r.raise_for_status()
 
     content = r.text
     lines = content.split('\n')
     formatted_lines = []
-    genre = url.split('/')[-1].replace('.m3u', '')
-    formatted_lines.append(f"{genre},#genre#\n")
 
     for i in range(0, len(lines) - 1, 2):
         match = re.search('"(.*?)",', lines[i])
         if match:
             name = match.group(1)
             link = lines[i + 1].strip()
-            formatted_lines.append(f"{name},{link}\n")
-    
-    with open(filename, 'a') as f:
-        f.write('\n'.join(formatted_lines))
+            formatted_lines.append(f"{name},{link}")
 
-# Copy content from gxtv.txt to iptv2.txt
-with open('gxtv.txt', 'r') as f1, open('iptv2.txt', 'w') as f2:
-    f2.write(f1.read())
+    return '\n'.join(formatted_lines)
 
-# The list of m3u file links to download and convert
+
 m3u_links = [
     'https://raw.githubusercontent.com/iptv-org/iptv/master/streams/kr.m3u',
     'https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/IPTV.m3u',
     'https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/bestv.m3u'
 ]
 
+# Copy content from gxtv.txt to iptv2.txt
+with open('gxtv.txt', 'r') as f1, open('iptv2.txt', 'w') as f2:
+    f2.write(f1.read())
+
+# Download and convert each m3u file, then append to iptv2.txt
 for link in m3u_links:
-    download_and_convert(link, 'iptv2.txt')
+    filename = os.path.basename(link)
+    name, _ = os.path.splitext(filename)
+    formatted_content = download_and_convert(link)
+    with open('iptv2.txt', 'a') as f:
+        f.write(name + ',#genre#\n' + formatted_content + '\n')
