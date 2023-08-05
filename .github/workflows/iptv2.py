89aa93c25eed792c8c9f44ddb7fb40984fa2cd0d
cuijizhu <60956208+cuijizhu@users.noreply.github.com>
2023-08-05 21:36:14 +0800

    Update iptv2.py

--- .github/workflows/iptv2.py
+++ .github/workflows/iptv2.py
@@ -1,35 +1,42 @@
 import requests
-import re
 
-def download_and_convert(url):
-    r = requests.get(url)
-    r.raise_for_status()
+# 从给定的URL获取直播源内容
+def get_m3u_content(url):
+    response = requests.get(url)
+    return response.text
 
-    content = r.text
-    lines = content.split('\n')
-    formatted_lines = []
+# 将直播源内容转换成指定格式
+def convert_to_iptv2_format(m3u_content):
+    lines = m3u_content.splitlines()
+    converted_lines = []
 
-    for i in range(0, len(lines) - 1, 2):
-        match = re.search('"(.*?)",', lines[i])
-        if match:
-            name = match.group(1)
-            link = lines[i + 1].strip()
-            formatted_lines.append(f"{name},{link}")
+    for i in range(0, len(lines), 2):
+        info_line = lines[i].split(' tvg-id="', 1)[1].split('",', 1)[1].strip()
+        url_line = lines[i+1].strip()
+        converted_lines.append(f"{info_line},{url_line}")
 
-    return '\n'.join(formatted_lines)
+    return '\n'.join(converted_lines)
 
-m3u_links = [
-    'https://raw.githubusercontent.com/iptv-org/iptv/master/streams/kr.m3u',
-    'https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/IPTV.m3u',
-    'https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/bestv.m3u'
-]
+# 将转换后的内容保存到iptv2.txt文件
+def save_to_iptv2_file(content):
+    with open('iptv2.txt', 'w') as file:
+        file.write(content)
 
-# 下载和转换所有m3u链接的内容
-merged_content = ""
-for link in m3u_links:
-    formatted_content = download_and_convert(link)
-    merged_content += formatted_content + '\n'
+# 主函数
+def main():
+    urls = [
+        'https://raw.githubusercontent.com/iptv-org/iptv/master/streams/kr.m3u',
+        'https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/IPTV.m3u',
+        'https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/bestv.m3u'
+    ]
 
-# 将合并后的内容保存到iptv2.txt文件
-with open('iptv2.txt', 'w') as f:
-    f.write(merged_content)
+    merged_content = ""
+    for url in urls:
+        m3u_content = get_m3u_content(url)
+        converted_content = convert_to_iptv2_format(m3u_content)
+        merged_content += converted_content + '\n'
+
+    save_to_iptv2_file(merged_content)
+
+if __name__ == "__main__":
+    main()
