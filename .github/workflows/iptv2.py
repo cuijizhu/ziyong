066169f966430eef3ffb49e2ff7c250977591428
cuijizhu <60956208+cuijizhu@users.noreply.github.com>
2023-08-06 11:31:12 +0800

    Update iptv2.py

--- .github/workflows/iptv2.py
+++ .github/workflows/iptv2.py
@@ -1,16 +1,15 @@
 import requests
-import re
 
 URLS = [
-    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/kr.m3u",
-    "https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/IPTV.m3u",
-    "https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/bestv.m3u"
+    ("https://raw.githubusercontent.com/iptv-org/iptv/master/streams/kr.m3u", "韩国IPTV,#genre#"),
+    ("https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/IPTV.m3u", "4K/8K,#genre#"),
+    ("https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/bestv.m3u", "百事通,#genre#")
 ]
 
-def extract_channels(content):
+def extract_channels(content, genre):
     lines = content.split("\n")
-    channels = []
-    
+    channels = [genre]  # 把类型作为第一行加入
+
     for i in range(len(lines)):
         if lines[i].startswith("#EXTINF"):
             parts = lines[i].rsplit(",", 1)  # 使用rsplit从右边分隔一次，这样就能获取最后一个逗号后面的内容
@@ -23,18 +22,17 @@ def extract_channels(content):
 def main():
     all_channels = []
 
-    for url in URLS:
-        response = requests.get(url)
-        all_channels.extend(extract_channels(response.text))
-
     with open("gxtv.txt", "r") as file:
         existing_channels = file.readlines()
+        all_channels.extend(existing_channels)
+
+    for url, genre in URLS:
+        response = requests.get(url)
+        all_channels.extend(extract_channels(response.text, genre))
 
     with open("iptv2.txt", "w") as file:
         for channel in all_channels:
             file.write(channel + "\n")
-        for channel in existing_channels:
-            file.write(channel)
 
 if __name__ == "__main__":
     main()
